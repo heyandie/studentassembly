e338103add9b4fa2662c2b4fb2a14bcd6b025032
Andie Rabino <rabinoandie@gmail.com>
2016-02-08 13:27:16 +0800

    initial commit

---
+++ studentassembly/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for studentassembly project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studentassembly.settings")
+
+from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
+application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
