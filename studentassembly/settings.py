f078741b3162d741447f6f0b81bf13acebc715ba
Andie Rabino <rabinoandie@gmail.com>
2016-03-14 15:50:01 +0800

    added middleware to redirect http to https

--- studentassembly/settings.py
+++ studentassembly/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'studentassembly.middleware.SSLMiddleware',
 ]
 
 ROOT_URLCONF = 'studentassembly.urls'
