b34a1c3f8582779f0af9754fb96218ed7d97b66a
Andie Rabino <rabinoandie@gmail.com>
2016-04-06 18:03:31 +0800

    return token only on token auth

--- account/api.py
+++ account/api.py
@@ -54,8 +54,6 @@ def jwt_payload_handler(user):
         'user_id': str(user.id.hex),
         'email': user.email,
         'username': username,
-        'alias': user.username,
-        'report_count': user.report_count,
         'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA
     }
 
@@ -79,8 +77,7 @@ def jwt_payload_handler(user):
 
 def jwt_response_payload_handler(token, user=None, request=None):
     return {
-        'token': token,
-        'user': AccountSerializer(user).data
+        'token': token
     }
 
 
